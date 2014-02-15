a618290b5666daab62b7a71c00c6ca9761895c61
Marielle Volz <marielle.volz@gmail.com>
2014-02-15 15:52:17 +0000

    converting to more OO style, kind of a mess

--- parsexml.py
+++ parsexml.py
@@ -12,12 +12,18 @@ class Phenotypes():
     '''
     Information about phenotypes
     '''
+    pass
     
 class Trait():
     '''
     Information about traits
     '''
-    pass
+    def __init__(self, name, wlink, supertrait, subtrait, description):
+        self.name = name
+        self.wlink = wlink
+        self.supertrait = supertrait
+        self.subtrait = subtrait
+        self.description = description
 
 class Genotype():
     #genotype = 
@@ -53,39 +59,14 @@ def returnSNPs(xmlfile):
             snps.append(s.find("rsid").text)
     #print snps
     return snps
-    
-
-def determineOutputType(xmlfile):
-    #outdated code below. 
-    #else:
-        #try:
-            #otype = root.attrib['type']
-        #except KeyError:
-            #print 'Output tags must have a \'type\' attribute'
-            #return
-        #finally:
-            #if otype == 'phenotype':
-                #print otype
-                #phenotype(root)
-            #elif otype == 'probability':
-                #print otype
-                #probability(root)
-            #else:
-                #print 'Currently the only allowed values for the type attribute are \'phenotype\' and \'probability\''
-
-    pass
         
-def gettrait(root):
+def get_trait_dict(root):
     '''
-    Gets information about the trait.
-    This is standardized in both the probability.xml and phenotype.xml
+    Returns a dict of info about the trait. 
+    Will include any tag found wwithin the trait tag, unlike get_trait_obj
+    Probably will be made redundant by get_trait_obj
     '''
     
-    '''below are different ways of getting trait info'''
-    
- 
-    #get child elements of trait tag
-
     trait = root.getiterator("trait")[0]
 
     traitdict = {}
@@ -94,32 +75,27 @@ def gettrait(root):
     
     for key, value in traitdict.items():
         print "%s: %s\n" % (key,value)
-    
-    #another way- gets only tags specified in unstrucuredtags lists
-    #list of tags of which there is only one in the file and describe the trait
-    #create a dict of these and their values
-    #no error handling here, beware.
-    unstructtagdict = {}
-    unstructuredtags = ["name", "wikipedialink", "supertrait", "subtrait"]
-    for t in unstructuredtags:
-        unstructtagdict[t] = gettext(root,t)
-    #print unstructtagdict
-    
-    #alternatively these can be made into python variables
+        
+    return traitdict
+
+def get_trait_obj(root):
+    '''
+    Gets information about the trait and returns Trait object
+    '''
     name = gettext(root, "name")
     wlink = gettext(root,"wikipedialink")
     supertrait = gettext(root,"supertrait")
     subtrait = gettext(root,"subtrait")
     
-    #the description may have subelements (likely citations) so the element
-    #is returned here, instead of text.
     try:
         description = root.getiterator("description")[0]
     except:
         description = None
-        
-    return traitdict
     
+    trait = Trait(name = name, wlink = wlink, supertrait = supertrait, subtrait = subrait, description = description)
+
+    return trait
+     
 def gettext(root, text):
     '''
     returns variable corresponding to text within a tag
@@ -143,6 +119,19 @@ def getreports(root):
     #print report_list
     return report_list
 
+def getgenotypes(ethnicity):
+    '''
+    return dict of genotypes and meanings
+    '''
+    genotype_dict = {}
+    genotypes = ethnicity.findall("genotype")
+    for g in genotypes:
+        genotype = g.get('value')
+        meanings = {}
+        for child in g:
+            meanings[child.tag] = child.text.strip() 
+        genotype_dict[genotype] = meanings
+    return genotype_dict
 
 def reportify_phenotype(o_tag):
     '''
@@ -170,23 +159,8 @@ def reportify_phenotype(o_tag):
     
     #print report
     return report
-
-def getgenotypes(ethnicity):
-    '''
-    return dict of genotypes and meanings
-    '''
-    genotype_dict = {}
-    genotypes = ethnicity.findall("genotype")
-    for g in genotypes:
-        genotype = g.get('value')
-        meanings = {}
-        for child in g:
-            meanings[child.tag] = child.text.strip() 
-        genotype_dict[genotype] = meanings
-    return genotype_dict
-
         
-def reportify_probability(root):
+def reportify_probability(o_tag):
     '''
     Parses an output tag where a probability
     of the phenotype or disease described in the <outcome> tag.
@@ -197,9 +171,6 @@ def reportify_probability(root):
 
 if  __name__ =='__main__':
 
-    #loadxml('xml schemas/phenotype.xml')
-    #loadxml('xml schemas/probability.xml')
     returnSNPs("xml files/eye_color.xml")
-    #loadxml("xml files/spherical_errors.xml")
-    
+
     
