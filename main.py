514d7673c383bb21a2bf05e5700d2d146f4859b7
Marielle Volz <marielle.volz@gmail.com>
2014-05-13 17:40:02 +0100

    refactor

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import loadgenome as lg
 import parse as prs
+import makepdf as mpdf
 
-print lg.loadgen("sample genomes/23andme_sample.txt")
+#print lg.loadgen("sample genomes/23andme_sample.txt")
+mpdf.go("sample genomes/23andme_sample.txt")
